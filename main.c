7ff5a9756dc877bd5b885cada9adc99d6c42c4cc
conmarap <conmarap@gmail.com>
2017-11-20 23:05:09 -0500

    Edit main to make into a command line util

--- main.c
+++ main.c
@@ -21,6 +21,16 @@ char *convertToNumberingScheme(int number) {
 }
 
 int main(int argc, char **argv) {
+    char *arg = "";
+
+    if (argc > 1) {
+        arg = argv[1];
+    }
+
+    char *result = pyrin(arg);
+    printf("%s\n", to_hex(result));
+
+    /*
     // A single letter.
     char *result = pyrin("a");
     printf("%s\n", to_hex(result));
@@ -49,4 +59,5 @@ int main(int argc, char **argv) {
     // Same input as above only without a period at the end.
     result = pyrin("Lorem ipsum dolor sit amet, nunc leo consectetuer elit velit, id sapien, egestas consectetuer purus in, vel ipsum curabitur lorem amet. Enim non massa, a nulla et cras erat egestas. Tellus nec ipsum maecenas placerat, in curae lacinia. A donec. Duis ut dolor turpis eget suspendisse, lacus diam ante aliquam dolor posuere, dolor sed lacinia consequat augue condimentum, sollicitudin wisi tristique lectus vel. Pellentesque commodo. Non dui tellus nunc sed aliquam. Amet turpis tincidunt sapien vel duis. Tellus vel nam ipsum pulvinar, etiam pede tellus nam donec, mollis elementum. Sapien ac, per nec non magna risus, sed at donec sit fusce. Sed hendrerit vestibulum sed venenatis torquent, sed tellus scelerisque aliquam. Ut vel lectus, sed quis non sit justo, vitae quisque, non placerat metus, donec sed sagittis velit magna");
     printf("%s\n", to_hex(result));
+    */
 }
